78b0f07d69462604fe7bb21ddcbd0be148460a8d
vgucsd <vgandari@eng.ucsd.edu>
2021-05-22 15:15:37 -0700

    combine operations within cycles

--- csdl/solvers/solver.py
+++ csdl/solvers/solver.py
@@ -212,15 +212,6 @@ class Solver(object):
             default=False,
             desc='Set to True to record residuals at the solver level')
         self.recording_options.declare(
-            'record_metadata',
-            types=bool,
-            desc='Deprecated. Recording '
-            'of metadata will always be done',
-            deprecation="The recording option, record_metadata, on "
-            "Solver is "
-            "deprecated. Recording of metadata will always be done",
-            default=True)
-        self.recording_options.declare(
             'includes',
             types=list,
             default=['*'],
