324c14df25951295ec78c7bc282b38a5c1ee369b
vgucsd <vgandari@eng.ucsd.edu>
2021-05-30 15:44:52 -0600

    rename ex/test _indep_var

--- csdl/tests/test_input.py
+++ csdl/tests/test_input.py
@@ -2,9 +2,9 @@ import numpy as np
 import pytest
 
 
-def test_indep_var(name):
-    from csdl.examples.valid.ex_indep_var_simple import example
-    exec('from {} import Simulator'.format(name))
+def test_input(backend):
+    from csdl.examples.valid.ex_input_simple import example
+    exec('from {} import Simulator'.format(backend))
     sim = example(eval('Simulator'))
     np.testing.assert_approx_equal(sim['z'], 10.)
     result = sim.check_partials(out_stream=None,
