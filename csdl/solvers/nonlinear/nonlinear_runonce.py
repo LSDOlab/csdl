db46486682b0482226ff1c467a1c7e1755270d83
vgandari <victor.gandarillas@icloud.com>
2021-06-06 16:12:34 -0700

    clean up solvers

--- csdl/solvers/nonlinear/nonlinear_runonce.py
+++ csdl/solvers/nonlinear/nonlinear_runonce.py
@@ -3,9 +3,7 @@ Define the NonlinearRunOnce class.
 
 This is a simple nonlinear solver that just runs the system once.
 """
-from openmdao.recorders.recording_iteration_stack import Recording
-from openmdao.solvers.solver import NonlinearSolver
-from openmdao.utils.mpi import multi_proc_fail_check
+from csdl.solvers.nonlinear_solver import NonlinearSolver
 
 
 class NonlinearRunOnce(NonlinearSolver):
@@ -17,27 +15,6 @@ class NonlinearRunOnce(NonlinearSolver):
 
     SOLVER = 'NL: RUNONCE'
 
-    def solve(self):
-        """
-        Run the solver.
-        """
-        system = self._system()
-
-        with Recording('NLRunOnce', 0, self) as rec:
-            # If this is a parallel group, transfer all at once then run each subsystem.
-            if len(system._subsystems_myproc) != len(system._subsystems_allprocs):
-                system._transfer('nonlinear', 'fwd')
-
-                with multi_proc_fail_check(system.comm):
-                    for subsys in system._subsystems_myproc:
-                        subsys._solve_nonlinear()
-
-            # If this is not a parallel group, transfer for each subsystem just prior to running it.
-            else:
-                self._gs_iter()
-
-            rec.abs = 0.0
-            rec.rel = 0.0
 
     def _declare_options(self):
         """
