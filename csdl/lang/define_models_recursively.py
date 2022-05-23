443107d96a5f3fa3c39afd0573060476509f1f6a
vgucsd <vgandari@eng.ucsd.edu>
2022-05-22 17:26:56 -0700

    update define_model_recursively

--- csdl/lang/define_models_recursively.py
+++ csdl/lang/define_models_recursively.py
@@ -4,50 +4,50 @@ except ImportError:
     pass
 from csdl.lang.concatenation import Concatenation
 from warnings import warn
+from typing import Set
 
 
-def define_recursively(model: 'Model') -> 'Model':
+def define_models_recursively(model: 'Model'):
     """
     Construct intermediate representation for each model in the
-    hierarchy from the bottom up
+    hierarchy from the top down
     """
-    for s in model.subgraphs:
-        m = s.submodel
-        _ = define_recursively(m)
-        if m.defined is False:
-            m.define()
-            m.defined = True
-
     if model.defined is False:
+        # this does nothing if user defines model inline; whether or not
+        # user defines model inline, submodels are not defined until
+        # this context
         model.define()
         model.defined = True
+        # Models must be defined from the top down because ubgraphs are
+        # not added to the model until after the model is defined
+        for s in model.subgraphs:
+            m = s.submodel
+            define_models_recursively(m)
 
         # check for empty model
         if model.registered_outputs == [] and model.subgraphs == []:
             if model.inputs == []:
                 raise ValueError(
-                    "This model doesn't do anything. Either register outputs, or create a model hierarchy."
+                    "This model doesn't do anything. Models must register inputs, outputs, or contain submodels."
                 )
             else:
                 warn(
-                    "This model only creates inputs for the top level model"
+                    "This model only registers inputs for the main model"
                 )
 
         # Check if all design variables are inputs
-        input_names = [inp.name for inp in model.inputs]
+        input_names: Set[str] = {inp.name for inp in model.inputs}
         for name in model.design_variables.keys():
             if name not in input_names:
                 raise KeyError(
                     "{} is not the CSDL name of an input to the model".
                     format(name))
-        del input_names
 
         # Check that all outputs are defined
         # for output in self.sorted_nodes:
         for output in model.registered_outputs:
             if isinstance(output, Concatenation):
                 if output.defined is False:
-                    raise ValueError("Output not defined for {}".format(
-                        repr(output)))
-
-    return model
+                    raise ValueError(
+                        "Output not defined for {}. When defining a concatenation, at least one index must be defined in terms of another CSDL Variable"
+                        .format(repr(output)))
