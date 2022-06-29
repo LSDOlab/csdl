db4aa1295f4931bedefdd16cdc201db6c0ee2c18
vgucsd <vgandari@eng.ucsd.edu>
2022-06-29 09:28:54 -0700

    update dependencies

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ setup(
     packages=[
         'csdl',
     ],
-    # python_requires='>=3.10',
+    python_requires='>=3.10',
     install_requires=[
         'numpy',
         'pint',
         'guppy3',
         'networkx',
+        'pytest',
+        'scipy',
+        'matplotlib',
     ],
     tests_require=['pytest'],
     version_config=True,
