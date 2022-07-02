5bc63f9447a9430907d362755b903df093678777
vgucsd <vgandari@eng.ucsd.edu>
2022-07-01 19:17:05 -0700

    add variable bracket tests

--- csdl/tests/test_bracketed_search.py
+++ csdl/tests/test_bracketed_search.py
@@ -11,7 +11,7 @@ def test_solve_quadratic_bracketed_scalar(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_solve_quadratic_bracketed_array(backend):
@@ -26,7 +26,7 @@ def test_solve_quadratic_bracketed_array(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_bracketed_with_subsystems_scalar(backend):
@@ -41,7 +41,7 @@ def test_bracketed_with_subsystems_scalar(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_bracketed_with_subsystems_array(backend):
@@ -56,7 +56,7 @@ def test_bracketed_with_subsystems_array(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 # ----------------------------------------------------------------------
@@ -71,7 +71,7 @@ def test_solve_quadratic_bracketed_scalar_define_model_inlines(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_solve_quadratic_bracketed_array_define_model_inlines(backend):
@@ -86,7 +86,7 @@ def test_solve_quadratic_bracketed_array_define_model_inlines(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_bracketed_with_subsystems_scalar_define_model_inlines(backend):
@@ -101,7 +101,7 @@ def test_bracketed_with_subsystems_scalar_define_model_inlines(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
 
 
 def test_bracketed_with_subsystems_array_define_model_inlines(backend):
@@ -116,4 +116,121 @@ def test_bracketed_with_subsystems_array_define_model_inlines(backend):
     result = sim.check_partials(out_stream=None,
                                 compact_print=True,
                                 method='cs')
-    # sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_solve_quadratic_bracketed_scalar_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_bracketed_scalar_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(sim['x'], np.array([1.0]))
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_solve_quadratic_bracketed_array_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_bracketed_array_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['x'],
+        np.array([1.0, 3.0]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_bracketed_with_subsystems_scalar_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_with_subsystems_bracketed_scalar_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['y'],
+        np.array([1.04458331]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_bracketed_with_subsystems_array_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_with_subsystems_bracketed_array_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['y'],
+        np.array([1.04458331, 2.65947684]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+# ----------------------------------------------------------------------
+
+
+def test_solve_quadratic_bracketed_scalar_define_model_inlines_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_bracketed_scalar_define_model_inline_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(sim['x'], np.array([1.0]))
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_solve_quadratic_bracketed_array_define_model_inlines_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_bracketed_array_define_model_inline_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['x'],
+        np.array([1.0, 3.0]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_bracketed_with_subsystems_scalar_define_model_inlines_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_with_subsystems_bracketed_scalar_define_model_inline_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['y'],
+        np.array([1.04458331]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
+
+
+def test_bracketed_with_subsystems_array_define_model_inlines_variable_brackets (backend):
+    from csdl.examples.valid.ex_bracketed_search_with_subsystems_bracketed_array_define_model_inline_variable_brackets import example
+    exec('from {} import Simulator'.format(backend))
+    sim, rep = example(eval('Simulator'))
+    np.testing.assert_almost_equal(
+        sim['y'],
+        np.array([1.04458331, 2.65947684]),
+    )
+
+    result = sim.check_partials(out_stream=None,
+                                compact_print=True,
+                                method='cs')
+    sim.assert_check_partials(result, atol=1.e-6, rtol=1.e-6)
